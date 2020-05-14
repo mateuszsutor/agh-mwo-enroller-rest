78f2d6d8fb4e1c64c8db20c7ebedfcb24c0e7a4b
Mateusz Sutor <msutor@student.agh.edu.pl>
2020-05-14 22:45:02 +0200

    Creating a new "MeetingRestController" and the ability to downloads all meetings.

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -3,6 +3,7 @@ package com.company.enroller.persistence;
 import java.util.Collection;
 
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
@@ -11,16 +12,13 @@ import com.company.enroller.model.Meeting;
 @Component("meetingService")
 public class MeetingService {
 
-	DatabaseConnector connector;
+	Session session;
 
 	public MeetingService() {
-		connector = DatabaseConnector.getInstance();
+		session = DatabaseConnector.getInstance().getSession();
 	}
 
 	public Collection<Meeting> getAll() {
-		String hql = "FROM Meeting";
-		Query query = connector.getSession().createQuery(hql);
-		return query.list();
+		return session.createCriteria(Meeting.class).list();
 	}
-
 }
